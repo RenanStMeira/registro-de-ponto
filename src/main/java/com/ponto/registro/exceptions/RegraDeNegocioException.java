f2a1eec763a00a1c6ebc0171591890f6ecd3b11d
RenanStMeira <renan.meira25@gmail.com>
2024-04-14 17:54:27 -0300

    feat: configurando projeto

---
+++ src/main/java/com/ponto/registro/exceptions/RegraDeNegocioException.java
@@ -0,0 +1,8 @@
+package com.ponto.registro.exceptions;
+
+public class RegraDeNegocioException extends Exception{
+
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
